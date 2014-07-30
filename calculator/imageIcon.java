72b45e598c9a49cb661e9b7b7b0d683e47d983fc
petrov86 <t_petrov@mail.bg>
2014-07-30 09:41:23 +0300

    Create imageIcon.java

---
+++ calculator/imageIcon.java
@@ -0,0 +1,40 @@
+package calucalotor;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+public class imageIcon extends Image {
+
+	@Override
+	public Graphics getGraphics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHeight(ImageObserver arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getProperty(String arg0, ImageObserver arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageProducer getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWidth(ImageObserver arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
