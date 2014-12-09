7b63bfa280c61cef51dfa6779554c1a95815cb69
petrov86 <t_petrov@mail.bg>
2014-12-09 11:14:01 +0200

    Update Background.java

--- Game-Alleyway/Background.java
+++ Game-Alleyway/Background.java
@@ -9,7 +9,7 @@ public class Background extends GameObject {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static final String BACKGROUND = "images/background.png";
+	private static final String BACKGROUND = "images/background.png";
 
 	public Background() {
 		x = 0;
